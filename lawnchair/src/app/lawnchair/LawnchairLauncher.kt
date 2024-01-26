02a66357b8b6241e4c5ccd6174cdd0fa51b3ab9d
Goooler <wangzongler@gmail.com>
2024-01-26 16:20:21 +0800

    Fix style

--- lawnchair/src/app/lawnchair/LawnchairLauncher.kt
+++ lawnchair/src/app/lawnchair/LawnchairLauncher.kt
@@ -22,8 +22,6 @@ import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.Display
 import android.view.View
 import android.view.ViewTreeObserver
